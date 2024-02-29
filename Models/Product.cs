93d2bb1132f7427887a9e3187a7a3b3169565a1c
sapirovadya <sapirov21@gmail.com>
2024-02-29 13:52:55 +0200

    Add product model

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GleamBoutiqueProject.Models
+{
+    public class Product
+    {
+        public int Pid { get; set; }
+        public string PName { get; set; }
+        public int OriginPrice { get; set; }
+        public int Amount { get; set; }
+        public int Notify_Count { get; set; }
+        public string category { get; set; }
+
+
+
+    }
+
+
+
+}
