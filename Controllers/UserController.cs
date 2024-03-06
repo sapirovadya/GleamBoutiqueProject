b9b04012acd02d3f0430750b381e33072a7d1423
oriyahazan <oriyaha.ac.sce.ac.il>
2024-03-06 15:13:11 +0200

    Create shop HTML page and shopcontroller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
- using GleamBoutiqueProject.Models;
+using GleamBoutiqueProject.Models;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
