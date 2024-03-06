7de10f4ff8724ea195435c77618532cbfd2a702d
mayzo <mayzohar2259@gmail.com>
2024-03-06 15:39:31 +0200

    Creat page signinup

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -22,39 +22,91 @@ namespace GleamBoutiqueProject.Controllers
 
 
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult SignInUp()
+        public IActionResult SignIn()
         {
             User newUser = new User();
+            newUser.Password = "";
             return View(newUser);
         }
 
-        public IActionResult UserSign(User myUser)
+        //public IActionResult UserSign(User myUser)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        using (SqlConnection connection = new SqlConnection(connectionString))
+        //        {
+        //            connection.Open();
+        //            string sqlQuery = "INSERT INTO [User] VALUES (@Value1, @Value2, @Value3, @Value4)";
+
+        //            using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+        //            {
+        //                //Set the parameter values to the SQL
+        //                command.Parameters.AddWithValue("@Value1", myUser.FirstName);
+        //                command.Parameters.AddWithValue("@Value2", myUser.LastName);
+        //                command.Parameters.AddWithValue("@Value3", myUser.Email);
+        //                command.Parameters.AddWithValue("@Value4", myUser.Password);
+
+        //                int rowsAffected = command.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                    return RedirectToAction("Index", "Home", myUser);
+        //                else
+
+        //                    return View("SignInUp", myUser);
+        //            }
+        //            connection.Close();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return View("SignInUp", myUser);
+        //    }
+        //}
+
+
+        public IActionResult SignIn(User myUser)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            // Check if the ModelState is valid for the sign-in form
+            if (ModelState.IsValid)
             {
-                connection.Open();
-                string sqlQuery = "INSERT INTO [User] VALUES (@Value1, @Value2, @Value3, @Value4)";
-
-                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    //Set the parameter values to the SQL
-                    command.Parameters.AddWithValue("@Value1", myUser.FirstName);
-                    command.Parameters.AddWithValue("@Value2", myUser.LastName);
-                    command.Parameters.AddWithValue("@Value3", myUser.Email);
-                    command.Parameters.AddWithValue("@Value4", myUser.Password);
+                    try
+                    {
+                        connection.Open();
+                        string query = "SELECT COUNT(*) FROM [User] WHERE Email = @Email AND Password = @Password";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@Email", myUser.Email);
+                        command.Parameters.AddWithValue("@Password", myUser.Password);
 
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                        return View("index", myUser);
-                    else
-                        return View("SignInUp", myUser);
+                        int count = (int)command.ExecuteScalar();
+                        if (count > 0)
+                        {
+                            // Authentication successful
+                            // Redirect to the home page or perform other actions
+                            return RedirectToAction("Index", "Home",myUser);
+                        }
+                        else
+                        {
+                            ModelState.AddModelError(string.Empty, "Invalid email or password");
+                            return View("SignIn", myUser);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError(string.Empty, "An error occurred: " + ex.Message);
+                        return View("SignIn", myUser);
+                    }
                 }
-                connection.Close();
+            }
+            else
+            {
+                // ModelState is not valid, return to the sign-in form
+                return View("SignIn", myUser);
             }
         }
 
