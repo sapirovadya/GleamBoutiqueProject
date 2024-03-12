af7d583ef847aa4921afd73120984c1ecf06c508
mayzo <mayzohar2259@gmail.com>
2024-03-12 22:58:39 +0200

    Add cart page and update a shop page and shopcontroller and create a cart viewmodel

---
+++ ViewModel/CartItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GleamBoutiqueProject.ViewModel
+{
+    public class CartItemViewModel
+    {
+        public string ProId { get; set; }
+        public int ProAmount { get; set; }
+        public string PName { get; set; }
+        public int OriginPrice { get; set; }
+        public int salePrice { get; set; }
+    }
+}
+
